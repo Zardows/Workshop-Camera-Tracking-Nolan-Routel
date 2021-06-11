3b3862048b92e08c7b86dd733913527d2e0ff3c0
nolan <nolan.routel@epitech.eu>
2021-06-11 16:32:02 +0400

    step 3 et 4

--- open_image.py
+++ open_image.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import cv2
+import mediapipe as mp
 
 
 def open_image():
@@ -16,6 +17,43 @@ def open_video():
     while True:
         ret, frame = cam.read()
         cv2.imshow("Image", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+def detect_hand():
+    cam = cv2.VideoCapture(0)
+    mpHand = mp.solutions.hands
+    hands = mpHand.Hands()
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("unable to open camera")
+            break
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb)
+        print(results.multi_hand_landmarks)
+        cv2.imshow("Camera", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+def draw_plan():
+    cam = cv2.VideoCapture(0)
+    mpHand = mp.solutions.hands
+    hands = mpHand.Hands()
+    mpDraw = mp.solutions.drawing_utils
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("unable to open camera")
+            break
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb)
+        if results.multi_hand_landmarks:
+            for handlms in results.multi_hand_landmarks:
+                results.draw_landmarks(frame, handlms)
+        cv2.imshow("Camera", frame)
         if cv2.waitKey(1) == ord('q'):
             break
     cv2.destroyAllWindows()
